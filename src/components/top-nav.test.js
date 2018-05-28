2e4017e64a20a0d6a5320b1117e7f47d016bf756
Cullan Bonilla <cullanbonilla@icloud.com>
2018-05-28 11:07:45 -0400

    added tests

--- src/components/top-nav.test.js
+++ src/components/top-nav.test.js
@@ -7,4 +7,14 @@ describe('<TopNav/>', () => {
     it('should render without crashing', () => {
         shallow(<TopNav/>);
     })
+
+    it('should fire callback when user selects new game', () => {
+        const callback = jest.fn();
+        const wrapper = shallow(<TopNav onRestartGame={callback}/>);
+        const link = wrapper.find('.new');
+        link.simulate('click', {
+            preventDefault() {}
+        })
+        expect(callback).toHaveBeenCalled(); 
+    })
 })
