34d7a71b45081c1969087a646648e130d0980102
Cullan Bonilla <cullanbonilla@icloud.com>
2018-05-28 08:22:40 -0400

    added tests for props & state

--- src/components/feedback.test.js
+++ src/components/feedback.test.js
@@ -7,4 +7,10 @@ describe('<Feedback/>', () => {
     it('should render without crashing', () => {
         shallow(<Feedback />);
     })
+
+    it('renders feedback', () => {
+        let testFeedback = 'This is some test feedback';
+        let wrapper = shallow(<Feedback feedback={testFeedback}/>);
+        expect(wrapper.contains(testFeedback)).toEqual(true);
+    })
 })
