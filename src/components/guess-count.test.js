3354f5c51f06a41e8b98ba32720b8460fd0520cc
Cullan Bonilla <cullanbonilla@icloud.com>
2018-05-28 08:36:11 -0400

    added tests for props & state

--- src/components/guess-count.test.js
+++ src/components/guess-count.test.js
@@ -7,4 +7,10 @@ describe('<GuessCount/>', () => {
     it('should render without crashing', () => {
         shallow(<GuessCount/>); 
     })
+
+    it('should render the correct guess count', () => {
+        const count = 3;
+        const wrapper = shallow(<GuessCount guessCount={count}/>);
+        expect(wrapper.text()).toEqual(`You've made ${count} guesses!`);
+    })
 })
