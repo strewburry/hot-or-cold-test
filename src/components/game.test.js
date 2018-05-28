0308e40b26cdfdc9ed6811f1828fb4af83629cc5
Cullan Bonilla <cullanbonilla@icloud.com>
2018-05-28 08:32:45 -0400

    added tests for events

--- src/components/game.test.js
+++ src/components/game.test.js
@@ -5,6 +5,47 @@ import Game from './game';
 
 describe('<Game/>', () => {
     it('should render without crashing', () => {
-        shallow(<Game />);
+        shallow(<Game/>);
+    })
+
+    it('should accept guesses', () => {
+        const wrapper = shallow(<Game/>);
+        wrapper.setState({
+            correctAnswer: 100
+        });
+
+        wrapper.instance().makeGuess(30);
+        expect(wrapper.state('guesses')).toEqual([30]);
+        expect(wrapper.state('feedback')).toEqual('You\'re Ice Cold...');
+
+        wrapper.instance().makeGuess(70);
+        expect(wrapper.state('guesses')).toEqual([30, 70]);
+        expect(wrapper.state('feedback')).toEqual('You\'re Cold...');
+
+        wrapper.instance().makeGuess(90);
+        expect(wrapper.state('guesses')).toEqual([30, 70, 90]);
+        expect(wrapper.state('feedback')).toEqual('You\'re Warm.');
+
+        wrapper.instance().makeGuess(99);
+        expect(wrapper.state('guesses')).toEqual([30, 70, 90, 99]);
+        expect(wrapper.state('feedback')).toEqual('You\'re Hot!');
+
+        wrapper.instance().makeGuess(100);
+        expect(wrapper.state('guesses')).toEqual([30, 70, 90, 99, 100]);
+        expect(wrapper.state('feedback')).toEqual('You got it!');
+    })
+
+    it('should restart game', () => {
+        const wrapper = shallow(<Game/>); 
+        wrapper.setState({
+            guesses: [5, 10, 15, 20],
+            feedback: 'You\'re either hot or cold.',
+            correctAnswer: 300
+        });
+        wrapper.instance().restartGame(); 
+        expect(wrapper.state('guesses')).toEqual([]);
+        expect(wrapper.state('feedback')).toEqual('Make your guess!');
+        expect(wrapper.state('correctAnswer')).toBeLessThanOrEqual(100);
+        expect(wrapper.state('correctAnswer')).toBeGreaterThanOrEqual(0);
     })
 })
